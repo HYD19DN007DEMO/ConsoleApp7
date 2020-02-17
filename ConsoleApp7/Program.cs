769212b0f7c17886493a8785568509df01c61620
A Gayatri <arisamgayatri@gmail.com>
2020-02-17 14:25:46 +0530

    Changed to ReadLine

--- ConsoleApp7/Program.cs
+++ ConsoleApp7/Program.cs
@@ -11,7 +11,7 @@ namespace ConsoleApp7
         static void Main(string[] args)
         {
             Console.WriteLine("Hello");
-            Console.ReadKey();
+            Console.ReadLine();
         }
     }
 }
